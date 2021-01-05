7934aa2478919165c61a0aa0639b878b54705844
development 06 <1090000718@qq.com>
2021-01-05 18:39:00 +0800

    shrio-intro-auth

---
+++ shiro-intro-auth/src/main/java/top/gumt/shirointroauth/ShiroIntroAuthApplication.java
@@ -0,0 +1,5 @@
+package top.gumt.shirointroauth;
+
+public class ShiroIntroAuthApplication {
+
+}
---
+++ shiro-intro-auth/src/test/java/top/gumt/shirointroauth/ShiroIntroAuthApplicationTests.java
@@ -0,0 +1,10 @@
+package top.gumt.shirointroauth;
+
+import org.junit.Test;
+
+public class ShiroIntroAuthApplicationTests {
+    @Test
+    public void test() {
+
+    }
+}
