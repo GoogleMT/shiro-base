09a261cfe2699894ed58b786b8e2959373750aaa
GoogleMT <2365897256@qq.com>
2021-01-18 21:07:49 +0800

    shiro 笔记

---
+++ shiro-sessionmgr/src/main/java/top/gumt/shirosessionmgr/utils/JdbcTemplateUtils.java
@@ -0,0 +1,28 @@
+package top.gumt.shirosessionmgr.utils;
+
+import com.alibaba.druid.pool.DruidDataSource;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+public class JdbcTemplateUtils {
+
+    private static JdbcTemplate jdbcTemplate;
+
+    public static JdbcTemplate jdbcTemplate() {
+        if(jdbcTemplate == null) {
+            jdbcTemplate = createJdbcTemplate();
+        }
+        return jdbcTemplate;
+    }
+
+    private static JdbcTemplate createJdbcTemplate() {
+
+        DruidDataSource ds = new DruidDataSource();
+        ds.setDriverClassName("com.mysql.jdbc.Driver");
+        ds.setUrl("jdbc:mysql://localhost:3306/shiro");
+        ds.setUsername("root");
+        ds.setPassword("12345678");
+
+        return new JdbcTemplate(ds);
+    }
+
+}
